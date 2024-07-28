349ebd3a148f7c0979003f542b13fa5934524ef9
S Revanth Shalon Raj <revanthshalon@gmail.com>
2024-07-28 19:42:38 +0530

    Adding Data'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use futures::executor::block_on;
-use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
+use sea_orm::*;
 use std::env;
+
+mod entities;
+use entities::{prelude::*, *};
+
 fn main() {
     let db_url: String =
         env::var("DATABASE_URL").expect("Environment variable 'DATABASE_URL' not found");
@@ -14,7 +18,7 @@ fn main() {
 async fn run(db_url: &str, db_name: &str) -> Result<(), DbErr> {
     let db = Database::connect(db_url).await?;
 
-    let _db = &match db.get_database_backend() {
+    let db = &match db.get_database_backend() {
         DbBackend::MySql => {
             db.execute(Statement::from_string(db.get_database_backend(), format!("CREATE DATABASE IF NOT EXISTS {}", db_name))).await?;
             let url = format!("{}/{}",db_url, db_name);
@@ -32,5 +36,27 @@ async fn run(db_url: &str, db_name: &str) -> Result<(), DbErr> {
             db
         }
     };
+    let happy_bakery = bakery::ActiveModel {
+        name: ActiveValue::Set("Happy Bakery".to_owned()),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+    let _res = Bakery::insert(happy_bakery).exec(db).await?;
+
+    let la_boulangerie = bakery::ActiveModel {
+        name: ActiveValue::Set("La Boulangerie".to_owned()),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+    let bakery_res = Bakery::insert(la_boulangerie).exec(db).await?;
+    for chef_name in ["Jolie", "Charles", "Madeleine", "Frederic"] {
+        let chef = chef::ActiveModel {
+            name: ActiveValue::Set(chef_name.to_owned()),
+            bakery_id: ActiveValue::Set(bakery_res.last_insert_id),
+            ..Default::default()
+        };
+        Chef::insert(chef).exec(db).await?;
+    }
+    // println!("{:?}", bakery_res.last_insert_id);
     Ok(())
 }
